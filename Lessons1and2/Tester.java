7568527b56b69d6318ec0a99d712efc343451741
smithbwlhs <88860661+smithbwlhs@users.noreply.github.com>
2023-09-19 15:20:26 -0700

    Update Tester.java

--- Lessons1and2/Tester.java
+++ Lessons1and2/Tester.java
@@ -1,14 +1,17 @@
-public class Tester{
-  public static void main(String[] args){
-    //make a rectangle
+public class Tester {
+    public static void main(String[] args) {
+        //Type name = new Contructor()
+        Rectangle myRectangle = new Rectangle(5,6);
+        int myRectangleArea = myRectangle.getArea();
+        int myRectanglePerimeter = myRectangle.getPerimeter();
+        System.out.println("Area: " + myRectangleArea);
+        System.out.println("Perimeter: " + myRectanglePerimeter);
 
-
-    //make a sandwich
-    
-
-    //try different things
-    
-  }
-
-
+        //Make a new rectangle with a width of 12 and height of 10
+        Rectangle newRect = new Rectangle(12,10);
+        int newPerimeter = newRect.getPerimeter();
+        int newArea = newRect.getArea();
+        System.out.println("Area: " + newArea);
+        System.out.println("Perimeter: " + newPerimeter);
+    }
 }
