e15a9caeedc9dd04a49ac76768138cfc5cfc07e8
smithbwlhs <88860661+smithbwlhs@users.noreply.github.com>
2023-09-19 09:11:07 -0700

    Create Rectangle.java

---
+++ Lessons1and2/Rectangle.java
@@ -0,0 +1,24 @@
+public class Rectangle{
+
+    //properties of a rectangle
+
+
+    //rectangle constructor - you specify length and width
+
+    //rectangle constructor - default
+
+    //rectangle constructor - square
+
+
+    //get length
+
+
+    //get width
+
+    //set length
+  
+    //set width
+
+    
+
+}
