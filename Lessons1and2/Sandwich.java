06a10791ea25e5896a90936fe43b15bea9ff50ac
smithbwlhs <88860661+smithbwlhs@users.noreply.github.com>
2023-09-19 08:43:11 -0700

    Create Sandwich.java

---
+++ Lessons1and2/Sandwich.java
@@ -0,0 +1,21 @@
+public class Sandwich {
+  //what do sandwiches have?
+
+
+
+  //sandwich constructor - basic sandwich has bread
+
+
+  //sandwich constructor - default sandwich
+
+
+  //getter for some property
+
+
+  //setter for some property
+
+
+  //to string
+
+
+}
